52b1c6749555f639a1dd420b0c550cbbbd3eb0bd
ArthurVarela <arthurvarelalotti@gmail.com>
2024-09-02 14:52:57 -0300

    First Commit

---
+++ src/main/java/com/example/exerciciosvisualmix/ExerciciosvisualmixApplication.java
@@ -0,0 +1,13 @@
+package com.example.exerciciosvisualmix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosvisualmixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosvisualmixApplication.class, args);
+	}
+
+}
